0191ccb7ecf99d92a25bc9e127b18ccc5891d2dd
OliverDanger <od.oliverdixon@gmail.com>
2023-03-07 21:46:51 +0000

    wrote function that asserts if arrays are equal or not

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,11 +4,9 @@ const eqArrays = function (array1, array2) {
   let perfectScore = array1.length;
   let countScore = 0;
   if (array1.length !== array2.length) return false;
-  for (const item1 of array1) {
-    for (const item2 of array2) {
-      if (item1 === item2) {
-        countScore++;
-      }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      countScore++;
     }
   }
   if (countScore === perfectScore) {
@@ -17,4 +15,24 @@ const eqArrays = function (array1, array2) {
   return false;
 }
 
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays( array1, array2)) {
+    console.log(`🐢🐢🐢Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🦑🦑🦑Assertion Failed: ${array1} !== ${array2}`);
+  }
+}
+
+// const assertEqual = function(actual1, expected) {
+//   if (actual1 === expected) {
+//     console.log(`🐢🐢🐢Assertion Passed: ${actual1} === ${expected}`);
+//   } else {
+//     console.log(`🦑🦑🦑Assertion Failed: ${actual1} !== ${expected}`);
+//   }
+// };
+
 //TESTCODE
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([2, 3, 4], [1, 2, 3]);
+assertArraysEqual([], []);
