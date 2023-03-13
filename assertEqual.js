4b34b9121a5ef2ff883fead53b2c55b9d2870cba
OliverDanger <od.oliverdixon@gmail.com>
2023-03-13 17:55:45 +0000

    created seperate test files for assertEqual and head

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,4 @@ const assertEqual = function(actual1, expected) {
   }
 };
 
-//TEST CODE
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1,1);
-assertEqual('hi','hi');
-assertEqual(1,2);
+module.exports = assertEqual;
